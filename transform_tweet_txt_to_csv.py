84e2ef4965d8472a7e843c89b2b7f6c97ab49b5b
tamarlev <tamarlevy3@mail.tau.ac.il>
2020-05-09 13:55:41 +0300

    Add data pre processing

--- transform_tweet_txt_to_csv.py
+++ transform_tweet_txt_to_csv.py
@@ -2,6 +2,87 @@ import re
 import csv
 import pandas as pd
 import json
+import re
+import sys
+import warnings
+from cucco import Cucco
+import ftfy
+from nltk.stem.snowball import SnowballStemmer
+sbEng = SnowballStemmer('english')
+sbEsp = SnowballStemmer('spanish')
+
+# List of number terms
+nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven',
+        'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',
+        'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred',
+        'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion',
+        'septillion', 'octillion', 'nonillion', 'decillion',
+        'cero', 'uno', 'dos', 'Tres', 'cuatro', 'cinco', 'seis', 'ocho', 'nueve', 'diez', 'once',
+        'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'de diecisiete', 'Dieciocho', 'diecinueve',
+        'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'ochenta', 'noventa', 'cien']
+
+
+def remove_numbers(s):
+    """
+    Removes all numbers from strings, both alphabetic (in English) and numeric. Intended to be
+    part of a text normalisation process. If the number contains 'and' or commas, these are
+    left behind on the assumption the text will be cleaned further to remove punctuation
+    and stop-words.
+    """
+    digit = {str(i):'' for i in range(10)}
+    query = s.replace('-', ' ').lower().split(' ')
+    resultwords = [word for word in query if word not in nums]
+    noText = ' '.join(resultwords).encode('utf-8')
+    s = str(noText.decode("utf-8"))
+    noNums = s.translate(digit).replace('  ', ' ')
+    return noNums
+
+def clean_html(sentence):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, ' ', str(sentence))
+    return cleantext
+
+def clean_punc(sentence): #function to clean the word of any punctuation or special characters
+    cleaned = re.sub(r'[?|!|\'|"|#]',r'',sentence)
+    cleaned = re.sub(r'[.|,|)|(|\|/]',r' ',cleaned)
+    cleaned = cleaned.strip()
+    cleaned = cleaned.replace("\n"," ")
+    return cleaned
+
+def keep_alpha(sentence):
+    alpha_sent = ""
+    for word in sentence.split():
+        alpha_word = re.sub('[^a-z A-Z]+', ' ', word)
+        alpha_sent += alpha_word
+        alpha_sent += " "
+    alpha_sent = alpha_sent.strip()
+    return alpha_sent
+
+def lemmatisation(sentence):
+    sent = ' '.join([sbEng.stem(item) for item in (sentence).split(' ')])
+    return ' '.join([sbEsp.stem(item) for item in (sent).split(' ')])
+
+def remove_stop_words(sentence):
+    normEng = Cucco(language='en')
+    normEsp = Cucco(language='es')
+    norms = ['remove_stop_words', 'replace_punctuation', 'remove_extra_whitespaces']
+    sent = normEng.normalize(sentence, norms)
+    return normEsp.normalize(sent, norms)
+
+def fix_encoding(sentence):
+    return ftfy.fix_encoding(sentence)
+
+def clean_csv(csv):
+    csv_data = pd.read_csv(csv)
+    csv_data['tweet'] = csv_data['tweet'].str.lower()
+    csv_data['tweet'] = csv_data['tweet'].apply(clean_html)
+    csv_data['tweet'] = csv_data['tweet'].apply(clean_punc)
+    csv_data['tweet'] = csv_data['tweet'].apply(keep_alpha)
+    csv_data['tweet'] = csv_data['tweet'].apply(remove_stop_words)
+    csv_data['tweet'] = csv_data['tweet'].apply(remove_numbers)
+    csv_data['tweet'] = csv_data['tweet'].apply(lemmatisation)
+    csv_data['tweet'] = csv_data['tweet'].apply(fix_encoding)
+    csv_data.to_csv('cleaned_tweet_emojie.csv', index=False)
 
 def merge_csvs(csv1, csv2):
     """
@@ -58,12 +139,7 @@ def dictToCSV(PATH,EXPORTPATH):
             # if count % 10 == 0:
                 # print("\rConverted: ",count,"/",len(data),end="")
             # count += 1
-            line = {'tweet': tweet}
-            for label in fields:
-                if label == emoji:
-                    line[label] = 1
-                else:
-                    line[label] = 0
+            line = {'tweet': tweet, 'label':emoji}
             writer.writerow(line)
     print("\n>>>Finished successfully")
 
